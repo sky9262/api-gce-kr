7f2748d3b45ab3ff2b61e3a8b7e07512a6101efe
sky9262 <akash2002.pat@gmail.com>
2023-02-07 02:38:35 +0530

    update & fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-
+mongoose.set("strictQuery", false);
 const connectToMongo = ()=>{
     mongoose.connect(process.env.MONGODB_URI, ()=>{
         console.log("Connected to mongoose :-)");
